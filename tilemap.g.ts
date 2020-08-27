0af89cda20f727e389720e6a6efe7cc89061e705
AdrianaTadic7 <70153475+AdrianaTadic7@users.noreply.github.com>
2020-08-26 22:30:01 -0400

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -114,8 +114,6 @@ namespace myTiles {
     export const tile55 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile56 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile57 = image.ofBuffer(hex``);
 
 }
 // Auto-generated code. Do not edit.
